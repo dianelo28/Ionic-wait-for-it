fd6ed4d34232296bba5895845f86f83c46c39d0e
henry-yi <yoonho.yi88@gmail.com>
2015-09-03 16:54:57 -0700

    added models

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose"),
+	Schema = mongoose.Schema;
+
+var User = require("./user.js");
+
+var CommentSchema = new Schema({
+	comments: {type: String, expires: 60 * 15}
+	author: [{
+		type: Schema.Types.ObjectId,
+		ref: User 
+	}]
+});
+
+var Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
