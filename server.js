940e0fa92e142ae110d62db413b7d4334002390b
Diane Lo <Diane@Dianes-MacBook-Pro.local>
2015-09-08 10:37:18 -0700

    clean up

--- server.js
+++ server.js
@@ -74,11 +74,6 @@ function isAuthenticated(req, res, next) {
   })
 }
 
-app.get('/protected', isAuthenticated, function(req, res) {
-  // Prints currently signed-in user object
-  console.log(req.user);
-});
-
 app.post('/auth/login', function(req, res) {
   User.findOne({ email: req.body.email }, '+password').populate("favorites").exec(function(err, user) {
     if (!user) {
@@ -119,68 +114,6 @@ app.post('/auth/signup', function(req, res) {
           res.send({ token: token, user: user });
         });
       });
-    });
-  });
-});
-
-app.post('/auth/facebook', function(req, res) {
-  var accessTokenUrl = 'https://graph.facebook.com/v2.3/oauth/access_token';
-  var graphApiUrl = 'https://graph.facebook.com/v2.3/me';
-  var params = {
-    code: req.body.code,
-    client_id: req.body.clientId,
-    client_secret: "3d3329ef0e76d9c989b3483874c322bd",
-    redirect_uri: req.body.redirectUri
-  };
-
-  // Step 1. Exchange authorization code for access token.
-  request.get({ url: accessTokenUrl, qs: params, json: true }, function(err, response, accessToken) {
-    if (response.statusCode !== 200) {
-      return res.status(500).send({ message: accessToken.error.message });
-    }
-
-    // Step 2. Retrieve profile information about the current user.
-    request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile) {
-      if (response.statusCode !== 200) {
-        return res.status(500).send({ message: profile.error.message });
-      }
-      if (req.headers.authorization) {
-        User.findOne({ facebook: profile.id }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Facebook account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, "tokenSecret");
-          User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
-            }
-            user.facebook = profile.id;
-            user.picture = user.picture || 'https://graph.facebook.com/v2.3/' + profile.id + '/picture?type=large';
-            user.displayName = user.displayName || profile.name;
-            user.save(function() {
-              var token = createJWT(user);
-              res.send({ token: token });
-            });
-          });
-        });
-      } else {
-        // Step 3b. Create a new user account or return an existing one.
-        User.findOne({ facebook: profile.id }, function(err, existingUser) {
-          if (existingUser) {
-            var token = createJWT(existingUser);
-            return res.send({ token: token });
-          }
-          var user = new User();
-          user.facebook = profile.id;
-          user.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-          user.displayName = profile.name;
-          user.save(function() {
-            var token = createJWT(user);
-            res.send({ token: token });
-          });
-        });
-      }
     });
   });
 });
@@ -303,12 +236,6 @@ app.delete('/api/:userid/favorites', function(req, res) {
     found_user.save();
     res.json(found_user);
   });
-});
-
-app.get("/testing", function(req, res) {
-	User.find({}, function(err, found) {
-    res.json(found);
-  })
 });
 
 // listen on port 3000
--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-
 var app = angular.module('wait', ['ionic', 'ngMap', 'angularMoment', 'ngRoute', 'satellizer', 'ngCordova']);
 
 app.config(['$ionicConfigProvider', "$authProvider", function($ionicConfigProvider, $authProvider) {
@@ -12,20 +6,6 @@ app.config(['$ionicConfigProvider', "$authProvider", function($ionicConfigProvid
 
     $authProvider.loginUrl = 'http://localhost:3000/auth/login';
     $authProvider.signupUrl = 'http://localhost:3000/auth/signup';
-    $authProvider.facebook({
-      clientId: '865103580253448',
-      url: 'http://localhost:3000/auth/facebook',
-      authorizationEndpoint: 'https://www.facebook.com/v2.3/dialog/oauth',
-      redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
-      requiredUrlParams: ['display', 'scope'],
-      scope: ['email'],
-      scopeDelimiter: ',',
-      display: 'popup',
-      type: '2.0',
-      popupOptions: { width: 580, height: 400 }
-    });
-
-
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
@@ -94,12 +74,26 @@ app.run(["$rootScope", "$location", "$auth", function($rootScope, $location, $au
         if (!$auth.isAuthenticated() && next.requireAuth) {
             $rootScope.savedLocation = $location.url();
             $location.path('/login'); 
-        }
+        };
         if ($auth.isAuthenticated() && next.onBoard) {
             $location.path('/favorites');
-        }
-    })
-}])
+        };
+    });
+}]);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    };
+  });
+});
+
 
 app.controller('MainCtrl', ['$scope', '$rootScope', '$http', '$location', '$auth', function($scope, $rootScope, $http, $location, $auth){
 
@@ -111,34 +105,16 @@ app.controller('MainCtrl', ['$scope', '$rootScope', '$http', '$location', '$auth
       });
   };
 
-  // $scope.spots = YelpSearch.query();
-  // console.log($scope.spots);
-
-  // $scope.search() = function(){
-  //   $scope.restaurant
-  // }
-
-  // $scope.submitWait = function(marker, newWait){
-  //   marker.wait.push({
-  //     text: newWait,
-  //     time: new Date()
-  //   });
-
-  //   $scope.newWait = '';
-
-  // };
-
 }]);
 
 app.controller('HomeCtrl', ['$scope', '$rootScope', '$auth','$http', function($scope, $rootScope, $auth, $http){
-
+  //pass data from main controller
   $rootScope.spots = $scope.spots;
-
-
+  //map
   $scope.$on('mapInitialized', function (event, map) {
             $scope.objMapa = map;
          });
-
+  //info windows
   $scope.showInfoWindow = function (event, spot) {
             var infowindow = new google.maps.InfoWindow();
             var center = new google.maps.LatLng(spot.location.coordinate.latitude, spot.location.coordinate.longitude);
@@ -153,12 +129,11 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$auth','$http', function($s
             $scope.objMapa.setZoom(15);
             $scope.objMapa.setCenter(center);
          };
-         
-    $scope.isAuthenticated = function() {
-    //check if user is logged in
+  //check if user is authenticated
+  $scope.isAuthenticated = function() {
     return $auth.isAuthenticated();
   };
-
+  //favorites
   $scope.favorites = function(spot) {
     if (_.findWhere(JSON.parse(localStorage.currentUser).favorites, {business_id: spot.id}) != undefined) {
       var temp = JSON.parse(localStorage.currentUser);
@@ -181,7 +156,7 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$auth','$http', function($s
         });
     };  
   };
-
+  //check for favorites
   $scope.checkFavorites = function(spot) {
     if (_.findWhere(JSON.parse(localStorage.currentUser).favorites, {business_id: spot.id}) != undefined) {
       return "ion-ios-heart";
@@ -239,11 +214,12 @@ app.controller('FavCtrl', ['$scope','$http', function($scope, $http) {
 app.controller('BizCtrl', ['$scope', '$rootScope', '$ionicModal', '$http', '$routeParams', '$cordovaGeolocation', function($scope, $rootScope, $ionicModal, $http, $routeParams, $cordovaGeolocation){
   $scope.work = [];
   $scope.spot = {};
+  //get business info
   $http.get('http://localhost:3000/api/business/' + $routeParams.id)
       .then(function(response){
         $scope.spot = response.data;
         console.log($scope.spot)
-
+          //check location
           $scope.checkLocation = function(){
             var posOptions = {timeout: 10000, enableHighAccuracy: false};
               $cordovaGeolocation
@@ -259,7 +235,8 @@ app.controller('BizCtrl', ['$scope', '$rootScope', '$ionicModal', '$http', '$rou
                   }
 
                   console.log(spotCoord)
-                    if (lat == spotCoord.latitude && long == spotCoord.longitude)
+                  //check if user is close to location before letting them put in wait time
+                    if (lat != spotCoord.latitude && long != spotCoord.longitude)
                       $scope.waitTime = function(business){
                         $http.put('http://localhost:3000/api/business/' + $routeParams.id, business)
                           .then(function(response){
@@ -276,7 +253,19 @@ app.controller('BizCtrl', ['$scope', '$rootScope', '$ionicModal', '$http', '$rou
                 });
           };
       });
+  //commenting
+  $scope.newComment = function(comment) {
+    var postData = {comments: comment.content};
 
+    $http.post('http://localhost:3000/api/business/' + $routeParams.id +"/comments", postData)
+      .then(function(response){
+        $scope.work.push(response.data);
+        console.log($scope.work);
+      }, function(response) {
+        console.log("error " + response)
+      });
+  };
+  //modal for wait time
   $ionicModal.fromTemplateUrl('my-modal.html', {
     scope: $scope,
     animation: 'slide-in-up'
@@ -301,42 +290,11 @@ app.controller('BizCtrl', ['$scope', '$rootScope', '$ionicModal', '$http', '$rou
   $scope.$on('modal.removed', function() {
     // Execute action
   });
-      
-  $scope.newComment = function(comment) {
-    var postData = {comments: comment.content};
-
-    $http.post('http://localhost:3000/api/business/' + $routeParams.id +"/comments", postData)
-      .then(function(response){
-        $scope.work.push(response.data);
-        console.log($scope.work);
-      }, function(response) {
-        console.log("error " + response)
-      });
-  };
-
-  $scope.linkFacebook = function() {
-    // connect email account with instagram
-    $auth.link('facebook')
-      .then(function(response) {
-        $window.localStorage.currentUser = JSON.stringify(response.data.user);
-        $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
-      });
-  };  
 
 }]);
 
 app.controller('LoginCtrl', ['$scope', '$rootScope', '$window', '$auth', '$location', function($scope, $rootScope, $window, $auth, $location){
 
-    $scope.facebookLogin = function() {
-      $auth.authenticate('facebook')
-        .then(function(response) {
-          $window.localStorage.currentUser = JSON.stringify(response.data.user);
-          $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
-        })
-        .catch(function(response) {
-          console.log(response.data);
-        });
-    };
     $scope.emailLogin = function(sec) {
       $auth.login({ email: sec.email, password: sec.password })
         .then(function(response) {
@@ -381,18 +339,3 @@ app.controller('SignupCtrl', ['$scope', '$auth', '$location','$window','$rootSco
   };
 
 }]);
-
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    };
-
-    // $cordovaPlugin.someFunction().then(success, error);
-  });
-});
